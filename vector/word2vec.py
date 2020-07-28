dcfcf09e818cb728b13463bf6f3dbb4c37bd1f2d
frozsgy <frozsgy@gmail.com>
2020-07-28 21:43:02 +0300

    fixed word2vec

--- vector/word2vec.py
+++ vector/word2vec.py
@@ -5,7 +5,7 @@ from gensim.models import KeyedVectors
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-wv = KeyedVectors.load_word2vec_format(datapath(dir_path + "/" + "wikipedia-out-vector.bin"), binary=True)
+wv = KeyedVectors.load_word2vec_format(datapath(dir_path + "/" + "wikipedia-vector.bin"), binary=True)
 
 while True:
 	w = input("Enter word: ")
