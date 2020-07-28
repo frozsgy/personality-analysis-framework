6e644bd67a2374180c711b86dabbc074606553b2
frozsgy <frozsgy@gmail.com>
2020-07-28 19:46:11 +0300

    added word2vec related code

---
+++ vector/word2vec.py
@@ -0,0 +1,14 @@
+import os 
+
+from gensim.test.utils import datapath
+from gensim.models import KeyedVectors
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+wv = KeyedVectors.load_word2vec_format(datapath(dir_path + "/" + "wikipedia-out-vector.bin"), binary=True)
+
+while True:
+	w = input("Enter word: ")
+	print(wv[w])
+
+
