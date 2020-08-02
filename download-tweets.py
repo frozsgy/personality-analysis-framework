f3a935f2b54cc89e044836529d2d5a941f9af97a
frozsgy <frozsgy@gmail.com>
2020-08-02 18:28:00 +0300

    added tweet downloader

--- download-tweets.py
+++ download-tweets.py
@@ -5,15 +5,24 @@ from twitter.download import *
 
 def run(name):
     alltweets, outtweets = get_all_tweets(name, False, True)
-    create_csv(outtweets, name)
-    create_csv(alltweets, name, True)
-    print("Tweets have been downloaded succesfully")
+    if alltweets:
+        create_csv(outtweets, name)
+        create_csv(alltweets, name, True)
+        print("Tweets have been downloaded succesfully")
+    else :
+        print("No tweets found for user")
 
 
 if __name__ == "__main__":
-    args = sys.argv
+    """args = sys.argv
     if len(args) > 1 :
         username = args[1]
     else :
         name = input("Enter username to download tweets: ")
-    run(username)
+    run(username)"""
+
+    p = ['']
+    for i in p:
+        print("Downloading " + i)
+        run(i)
+        print("-"*80)
